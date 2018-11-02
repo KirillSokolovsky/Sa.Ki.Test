074e3ca07e610bab77d5707e6ddf46ac543b47a3
Kiryl Sakalouski <Kiryl_Sakalouski@epam.com>
2018-11-02 18:12:18 +0300

    Start work on Chrome control

---
+++ src/web/desktopApp/Sa.Ki.Test.WebAutomation.DesktopApp/JSB/BrowserFrame.cs
@@ -0,0 +1,36 @@
+﻿namespace Sa.Ki.Test.WebAutomation.DesktopApp.JSB
+{
+    using ReactiveUI;
+    using System;
+    using System.Collections.Generic;
+    using System.Collections.ObjectModel;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class BrowserFrame : ReactiveObject
+    {
+        public string Key { get; set; }
+
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => this.RaiseAndSetIfChanged(ref _name, value);
+        }
+
+        private long _identifier;
+        public long Identifier
+        {
+            get => _identifier;
+            set => this.RaiseAndSetIfChanged(ref _identifier, value);
+        }
+
+        private ObservableCollection<BrowserFrame> _frames;
+        public ObservableCollection<BrowserFrame> Frames
+        {
+            get => _frames;
+            set => this.RaiseAndSetIfChanged(ref _frames, value);
+        }
+    }
+}
---
+++ src/web/desktopApp/Sa.Ki.Test.WebAutomation.DesktopApp/JSB/main.js
@@ -0,0 +1,7 @@
+﻿(async function () {
+    await CefSharp.BindObjectAsync("boundAsync");
+    var r = boundAsync.div(16, 2)
+        .then(function (res) {
+            console.log(res);
+        });
+})();
